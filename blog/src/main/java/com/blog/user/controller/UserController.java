5e9819b3b54733806d9526fa1057593f0de246c9
silence <silence@172.30.211.3>
2016-04-07 09:43:18 +0800

    json test

--- blog/src/main/java/com/blog/user/controller/UserController.java
+++ blog/src/main/java/com/blog/user/controller/UserController.java
@@ -1,15 +1,22 @@
 package com.blog.user.controller;
 
+import com.blog.user.pojo.User;
 import com.silence.web.spring_min.anotation.Controller;
 import com.silence.web.spring_min.anotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
 	@RequestMapping("login.html")
-	public String login(){
-		System.out.println("go to login.....");
-		return "success";
+	public Object login(){
+		User user =new User();
+		user.setUserName("silence");
+		List<User> users=new ArrayList<>();
+		users.add(user);
+		return users;
 	}
 }
